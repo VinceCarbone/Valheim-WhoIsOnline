d7b466360a66835c294e421a8207beb06067e57d
Vince <VinnyCThatWhoIBe@gmail.com>
2025-02-25 23:32:43 -0600

    Improved player name detection and updated SteamName detection to pull directly from steamcommunity.com

--- Valheim-WhoIsOnline.ps1
+++ Valheim-WhoIsOnline.ps1
@@ -10,10 +10,14 @@ If(-not(Test-Path $logfile)){Write-Host "'$logfile' does not appear to be a vali
 
 Do{
     $content = Get-Content $logfile
+    # This builds an array containing only specific lines from the log file and then sorts them in order
+    $contentTrimmed = @($content -match "Got connection SteamID")
+    $contentTrimmed += @($content -match "I HAVE ARRIVED")
+    $contentTrimmed = @($contentTrimmed | Sort-Object)
 
-    # This section finds all the unique steamIDs in the log file
+    # This section finds all the unique steamIDs
     $steamIDs = @()
-    $steamIDLines = @($content | Select-String "Got connection SteamID") # finds every line in the log file that has a player connecting
+    $steamIDLines = @($contentTrimmed | Select-String "Got connection SteamID") # finds every line in the log file that has a player connecting
     ForEach($steamIDLine in $steamIDLines){$steamIDs += ($steamIDLine -split(' '))[-1]} # splits each line into a separate array, and selects the last item in the array (which is the steamid)
     $steamIDs = $steamIDs | Sort-Object | Get-Unique # removes duplicate steamIDs
 
@@ -21,24 +25,27 @@ Do{
     $Players = @()
     ForEach($steamID in $steamIDs){
         $Players +=@(      
-            $connection = (($content | Select-String "Got connection SteamID $steamID")[-1]).LineNumber # finds the last time this steamID connected
-            $LoggedOn = Get-Date((($content[$connection]).split(' ')[0] + " " + ($content[$connection]).split(' ')[1]) -replace ".$") # grabs the logon date from the line
+            $connection = $contentTrimmed.IndexOf((($contentTrimmed | Select-String "Got connection SteamID $steamID")[-1])) # finds the index of last time this steamID connected in the array
+            $LoggedOn = Get-Date((($contentTrimmed[$connection]).split(' ')[0] + " " + ($contentTrimmed[$connection]).split(' ')[1]) -replace ".$") # grabs the logon date from the relevant item in the array
             If($content | Select-string "Closing socket $steamID"){
                 # Putting this in an IF statement prevents an issue with a first time player who hasn't logged off before
                 $LoggedOff = Get-Date((((($content | Select-string "Closing socket $steamID")[-1]) -split ' ')[0]) + " " + (((($content | Select-string "Closing socket $steamID")[-1]) -split ' ')[1]) -replace ".$") # finds the last time this steamid logged off and grabs the date
             }Else{$LoggedOff = $null}
 
-            # This attempts to grab the player name from the logs
-            If($content[$connection + 4] -match "I HAVE ARRIVED!"){
+            # This attempts to grab the player name from the logs            
+            If($contentTrimmed[$connection + 1] -match "I HAVE ARRIVED!"){
                 # this attempts to work around an issue where you run the script while a player isn't fully connected.
-                $Player = (($content[$connection + 4]).split('>')[1]).replace("</color","")
+                $Player = (($contentTrimmed[$connection + 1]).split('>')[1]).replace("</color","")                
             }Else{$Player = $null}
 
-            # retrieves info from the interweb about who the steamID belongs to
-            $response = Invoke-WebRequest -Uri "https://steamid.io/lookup/$steamid"
-            If(($response -split "`n") | Select-String '                "name":'){
-                $SteamName = ((((($response -split "`n") | Select-String '                "name":') -split ",")[0] -replace '"name": "','') -replace '"','').trim()# gets the steam username from the $response variable
-            }Else{$SteamName = $null}
+            # Retrieves info from the interweb about who the steamID belongs to            
+            If(Test-Connection "steamcommunity.com" -Count 1 -ErrorAction SilentlyContinue){
+                $Response = Invoke-WebRequest -Uri "https://steamcommunity.com/profiles/$steamid"
+                If($Response){
+                    $SteamName = (((($response.content -split "`n") | Select-String "<title>") -replace "<title>Steam Community :: ","") -replace "</title>","").trim()
+                    If($SteamName -eq "Error"){$SteamName = $null}
+                }Else{$SteamName = $null}
+            }Else{$SteamName = $null}            
 
             # Builds the output
             [PSCustomObject]@{
